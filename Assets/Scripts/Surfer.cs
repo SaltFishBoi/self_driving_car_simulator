1c17e1eaa00b48b9ce613a9a7b8785eae32ee827
oshorab <omarshorab@gmail.com>
2021-03-10 08:34:44 -0800

    surfer added

---
+++ Assets/Scripts/Surfer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surfer : MonoBehaviour
+{
+    public Vector3 pointB;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Start()
+    {
+        var pointA = transform.position;
+        while (true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+        }
+    }
+
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i = 0.0f;
+        var rate = 0.5f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+}
+
+
+
+
+
